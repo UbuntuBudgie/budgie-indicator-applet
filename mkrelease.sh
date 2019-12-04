332887b861bf0d2495365fbe18d821bf281dff61
David Mohammed <fossfreedom@ubuntu.com>
2019-12-04 23:12:38 +0000

    Add mkrelease

---
+++ mkrelease.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+git submodule init
+git submodule update
+
+VERSION="0.6.2"
+NAME="budgie-indicator-applet"
+./git-archive-all --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
+xz -9 "${NAME}-${VERSION}.tar"
+
+gpg --armor --detach-sign "${NAME}-${VERSION}.tar.xz"
+gpg --verify "${NAME}-${VERSION}.tar.xz.asc"
+
