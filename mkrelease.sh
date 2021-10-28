07adbb03a673d4dcf8b58ad573a7d3e97c540ede
David Mohammed <fossfreedom@ubuntu.com>
2021-10-28 22:20:25 +0100

    Update mkrelease

--- mkrelease.sh
+++ mkrelease.sh
@@ -6,7 +6,7 @@ git submodule update
 
 VERSION="0.7.1"
 NAME="budgie-indicator-applet"
-git-archive-all --prefix ${NAME}-${VERSION}/ --verbose  ${NAME}-${VERSION}.tar
+git-archive-all.sh --prefix ${NAME}-${VERSION}/ --verbose  ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
 
 gpg --armor --detach-sign "${NAME}-${VERSION}.tar.xz"
