dd735d0f69dfc954995ac899f3483c9b9a7d443c
David Mohammed <fossfreedom@ubuntu.com>
2021-10-28 22:08:24 +0100

    v0.7.1

--- mkrelease.sh
+++ mkrelease.sh
@@ -4,7 +4,7 @@ set -e
 git submodule init
 git submodule update
 
-VERSION="0.7.0"
+VERSION="0.7.1"
 NAME="budgie-indicator-applet"
 git-archive-all --prefix ${NAME}-${VERSION}/ --verbose  ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
